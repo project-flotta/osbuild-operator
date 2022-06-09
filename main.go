e81413ebddc6b578835b1ac381d7e5c1af4fcca8
Jakub Dzon <jdzon@redhat.com>
2022-06-09 09:54:25 +0200

    Added OSBuildConfigTemplate controller
    Signed-off-by: Jakub Dzon <jdzon@redhat.com>

--- main.go
+++ main.go
@@ -126,6 +126,13 @@ func main() {
 		setupLog.Error(err, "unable to create webhook", "webhook", "OSBuildEnvConfig")
 		os.Exit(1)
 	}
+	if err = (&controllers.OSBuildConfigTemplateReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "OSBuildConfigTemplate")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
