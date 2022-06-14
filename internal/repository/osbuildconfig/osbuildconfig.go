9fa16e879c7fe7ac626b2cb8d16a9398ed4fb7f8
Jakub Dzon <jdzon@redhat.com>
2022-06-14 12:34:44 +0200

    Added repository method for retrieving OSBuildConfig by template name
    Signed-off-by: Jakub Dzon <jdzon@redhat.com>

--- internal/repository/osbuildconfig/osbuildconfig.go
+++ internal/repository/osbuildconfig/osbuildconfig.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	_ "github.com/golang/mock/mockgen/model"
 	"github.com/project-flotta/osbuild-operator/api/v1alpha1"
+	"github.com/project-flotta/osbuild-operator/internal/indexer"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -11,6 +12,7 @@ import (
 type Repository interface {
 	Read(ctx context.Context, name string, namespace string) (*v1alpha1.OSBuildConfig, error)
 	PatchStatus(ctx context.Context, osbuildConfig *v1alpha1.OSBuildConfig, patch *client.Patch) error
+	ListByOSBuildConfigTemplate(ctx context.Context, templateName string, namespace string) ([]v1alpha1.OSBuildConfig, error)
 }
 
 type CRRepository struct {
@@ -29,4 +31,16 @@ func (r *CRRepository) Read(ctx context.Context, name string, namespace string) 
 
 func (r *CRRepository) PatchStatus(ctx context.Context, osbuildConfig *v1alpha1.OSBuildConfig, patch *client.Patch) error {
 	return r.client.Status().Patch(ctx, osbuildConfig, *patch)
+}
+
+func (r *CRRepository) ListByOSBuildConfigTemplate(ctx context.Context, templateName string, namespace string) ([]v1alpha1.OSBuildConfig, error) {
+	configs := v1alpha1.OSBuildConfigList{}
+	err := r.client.List(ctx, &configs,
+		client.MatchingFields{indexer.ConfigByConfigTemplate: templateName},
+		client.InNamespace(namespace),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return configs.Items, nil
 }
