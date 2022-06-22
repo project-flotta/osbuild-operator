0f7b5deba80662f95ed35bc9b011c40590ac3df8
Jakub Dzon <jdzon@redhat.com>
2022-06-22 14:23:38 +0200

    Added OSBuild generation tests
    Signed-off-by: Jakub Dzon <jdzon@redhat.com>

---
+++ internal/manifests/manifests_suite_test.go
@@ -0,0 +1,13 @@
+package manifests_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestManifests(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Manifests Spec")
+}
---
+++ tests/matchers/matchers.go
@@ -0,0 +1,113 @@
+package matchers
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/golang/mock/gomock"
+	. "github.com/onsi/gomega"
+
+	"github.com/project-flotta/osbuild-operator/api/v1alpha1"
+)
+
+type osBuildConfigStatusMatcher struct {
+	expected *v1alpha1.OSBuildConfig
+}
+
+func NewOSBuildConfigStatusMatcher(expected *v1alpha1.OSBuildConfig) gomock.Matcher {
+	return osBuildConfigStatusMatcher{expected: expected}
+}
+
+func (o osBuildConfigStatusMatcher) Matches(other interface{}) bool {
+	if o.expected == nil || other == nil {
+		return reflect.DeepEqual(o.expected, other)
+	}
+
+	actual, ok := other.(*v1alpha1.OSBuildConfig)
+	if !ok {
+		return false
+	}
+
+	if !reflect.DeepEqual(actual.ObjectMeta, o.expected.ObjectMeta) {
+		return false
+	}
+
+	return reflect.DeepEqual(actual.Status, o.expected.Status)
+}
+
+func (o osBuildConfigStatusMatcher) String() string {
+	return fmt.Sprintf("is equal to %v (%T)", o.expected, o.expected)
+}
+
+type osBuildMatcher struct {
+	expected *v1alpha1.OSBuild
+}
+
+func NewOSBuildMatcher(expected *v1alpha1.OSBuild) gomock.Matcher {
+	return osBuildMatcher{expected: expected}
+}
+
+func (o osBuildMatcher) Matches(other interface{}) bool {
+	if o.expected == nil || other == nil {
+		return reflect.DeepEqual(o.expected, other)
+	}
+
+	actual, ok := other.(*v1alpha1.OSBuild)
+	if !ok {
+		return false
+	}
+
+	if !reflect.DeepEqual(actual.ObjectMeta, o.expected.ObjectMeta) {
+		return false
+	}
+
+	if !reflect.DeepEqual(actual.Spec.Kickstart, o.expected.Spec.Kickstart) {
+		return false
+	}
+
+	if actual.Spec.TriggeredBy != o.expected.Spec.TriggeredBy {
+		return false
+	}
+
+	return matchBuildDetails(actual.Spec.Details, o.expected.Spec.Details)
+}
+
+func (o osBuildMatcher) String() string {
+	return fmt.Sprintf("is equal to %v (%T)", o.expected, o.expected)
+}
+
+func matchBuildDetails(actualDetails v1alpha1.BuildDetails, expectedDetails v1alpha1.BuildDetails) bool {
+	if actualDetails.Distribution != expectedDetails.Distribution {
+		return false
+	}
+
+	if !reflect.DeepEqual(actualDetails.TargetImage, expectedDetails.TargetImage) {
+		return false
+	}
+
+	if actualDetails.Customizations == nil || expectedDetails.Customizations == nil {
+		return reflect.DeepEqual(actualDetails.Customizations, expectedDetails.Customizations)
+	}
+
+	if ok, err := ConsistOf(expectedDetails.Customizations.Packages).Match(actualDetails.Customizations.Packages); !ok || err != nil {
+		return false
+	}
+
+	if ok, err := ConsistOf(expectedDetails.Customizations.Users).Match(actualDetails.Customizations.Users); !ok || err != nil {
+		return false
+	}
+
+	if expectedDetails.Customizations.Services == nil || actualDetails.Customizations.Services == nil {
+		return reflect.DeepEqual(expectedDetails.Customizations.Services, actualDetails.Customizations.Services)
+	}
+
+	if ok, err := ConsistOf(expectedDetails.Customizations.Services.Enabled).Match(actualDetails.Customizations.Services.Enabled); !ok || err != nil {
+		return false
+	}
+
+	if ok, err := ConsistOf(expectedDetails.Customizations.Services.Disabled).Match(actualDetails.Customizations.Services.Disabled); !ok || err != nil {
+		return false
+	}
+
+	return true
+}
